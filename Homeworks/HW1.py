6e58fbc3be103759e6774264837292f7307bbe6d
Klewolas <48567287+Klewolas@users.noreply.github.com>
2021-02-17 21:01:33 +0300

    Ödev 1 Yüklendi.

--- Homeworks/HW1.py
+++ Homeworks/HW1.py
@@ -1,1 +1,21 @@
-aa
+import random
+
+kontrol = False #asal sayıyı tutan boolean.
+primeNumbers = []
+for j in range(2,100): #asal sayılar bulunması için for döngüleri.
+   for i in range(2,j):
+       if (j % i) == 0:  
+           break  
+   else:
+       kontrol = True
+   if kontrol == True:
+       primeNumbers.append(j)
+       kontrol = False
+        
+matrix = [[0 for i in range(3)] for j in range(3)] #matris oluşturuldu.
+
+for i in range(3): #oluşturulan matrisin içine rastgele asal sayılar atandı.
+    for j in range(3):
+        matrix[i][j] = primeNumbers[random.randint(0,len(primeNumbers))]
+        
+print(matrix)
